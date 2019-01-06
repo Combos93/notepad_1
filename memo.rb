4c170f126aa090755a2fdc6084c3766fd4ab7158
Combos93 <combos93@mail.ru>
2019-01-07 00:27:26 +0600

    v.0.2_worked

--- memo.rb
+++ memo.rb
@@ -1,9 +1,21 @@
 class Memo < Task
   def read_from_console
-    # todo
+    puts "Новая заметка (всё, что напишете до строчки \"end\"):"
+
+    @text = []
+    line = nil
+
+    until line == "end"
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop
   end
 
   def to_strings
-    # todo
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    @text.unshift(time_string)
   end
 end
